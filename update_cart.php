ec5c458886a8b8ad046d38347605795d6cd51d08
Werner Banyacskay <yox64@yahoo.com>
2015-06-24 18:38:19 +0300

    Update cart bugfix

--- update_cart.php
+++ update_cart.php
@@ -7,6 +7,21 @@ $product_id = $_GET['product_id'];
 
 if($action == 'add') {
 	$cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+	$found = false;
+
+	if(!empty($cart)) {
+		foreach($cart as $product) {
+			if($product == $product_id) {
+				$found = true;
+			}
+		}
+	}
+
+	if($found) {
+		header("Location: cart.php");
+		exit;
+	}
+
 	array_push($cart, $product_id);
 	$_SESSION['cart'] = $cart;
 }
